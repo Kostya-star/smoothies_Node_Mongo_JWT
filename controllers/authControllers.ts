0930ba96c6d992a26febc685b530db67bb41b358
Constantin <konstantindanilov19@hotmail.com>
2023-02-11 21:34:35 +0200

    made server give back specific errors in response when a user tries to sign in and log in

--- controllers/authControllers.ts
+++ controllers/authControllers.ts
@@ -1,30 +1,46 @@
-import { User } from "../models/User"
+import { User } from '../models/User';
+
+const handleErrors = (err) => {
+  let errors = { email: '', password: '' };
+
+  if(err.code === 11000) {
+    errors.email = 'This email already exists'
+    return errors
+  }
+
+  if (err.message.includes('user validation failed')) {
+    Object.values(err.errors).forEach(({ properties }: any) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+  return errors;
+};
 
 export const signup_get = (req, res) => {
-  res.render('signup')
-}
+  res.render('signup');
+};
 
 export const login_get = (req, res) => {
-
-  res.render('login')
-}
+  res.render('login');
+};
 
 export const signup_post = async (req, res) => {
   try {
-    const user = await User.create(req.body)
-    res.status(201).json(user)
-  } catch (error) {
-    res.status(400).send('Error, user not created')
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+    // if(error.)
   }
-}
+};
 
 export const login_post = (req, res) => {
-  
-  res.send('new login')
-}
+  res.send('new login');
+};
 
 // export {
-//   signup_get, 
+//   signup_get,
 //   login_get,
 //   signup_post,
 //   login_post
--- models/User.ts
+++ models/User.ts
@@ -1,17 +1,23 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose
 
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
 const userSchema = new Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, 'Please enter your email'],
     unique: true,
-    lowercase: true
+    lowercase: true,
+    validate: [(val) => emailRegex.test(val), 'Please enter a valid email'],
+    // trim: true,
+    // match: [emailRegex, 'Please enter a valid email']
   },
   password: {
     type: String,
-    required: true,
-    minLength: 6
+    required: [true, 'Please enter your password'],
+    minlength: [6, 'Password must be at least 6 characters'],
+    // select: false
   }
 })
 

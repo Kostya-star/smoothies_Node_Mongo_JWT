eb3a71df13a01b60dcc3998064e48df85fe2ec19
Constantin <konstantindanilov19@hotmail.com>
2023-02-13 13:26:06 +0200

    created sign up andlogin pages and styled them out

--- models/User.ts
+++ models/User.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt'
 const { Schema } = mongoose
 
 const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
@@ -9,16 +10,22 @@ const userSchema = new Schema({
     required: [true, 'Please enter your email'],
     unique: true,
     lowercase: true,
-    validate: [(val) => emailRegex.test(val), 'Please enter a valid email'],
-    // trim: true,
-    // match: [emailRegex, 'Please enter a valid email']
+    // validate: [(val) => emailRegex.test(val), 'Please enter a valid email'],
+    trim: true,
+    match: [emailRegex, 'Please enter a valid email']
   },
   password: {
     type: String,
     required: [true, 'Please enter your password'],
     minlength: [6, 'Password must be at least 6 characters'],
-    // select: false
+    select: false
   }
+})
+
+userSchema.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt()
+  this.password = await bcrypt.hash(this.password, salt)
+  next()
 })
 
 export const User = mongoose.model('user', userSchema)

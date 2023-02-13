606c4886fdd6ff2184a545bfb40dc9325cef47bf
Constantin <konstantindanilov19@hotmail.com>
2023-02-13 21:30:48 +0200

    linked frontend login with backend and made errors displayable in case of errors

--- controllers/authControllers.ts
+++ controllers/authControllers.ts
@@ -1,19 +1,32 @@
 import { User } from '../models/User';
 import jwt from 'jsonwebtoken'
 
+//handle erros
 const handleErrors = (err) => {
   let errors = { email: '', password: '' };
+  //login_post erros
+  if(err.message === 'Incorrect email') {
+    errors.email = 'This email is not registered'
+  }
 
+  if(err.message === 'Incorrect password') {
+    errors.password = 'The password is not correct'
+  }
+
+  // signup_post errors
+  //duplicate error code
   if(err.code === 11000) {
     errors.email = 'This email already exists'
     return errors
   }
 
+  //validation errors
   if (err.message.includes('user validation failed')) {
     Object.values(err.errors).forEach(({ properties }: any) => {
       errors[properties.path] = properties.message;
     });
   }
+
   return errors;
 };
 
@@ -45,13 +58,15 @@ export const signup_post = async (req, res) => {
   }
 };
 
-export const login_post = (req, res) => {
-  res.send('new login');
+export const login_post = async (req, res) => {
+  const { email, password } = req.body
+  try {
+    const user = await User.login(email, password)
+    const token = onCreateToken(user._id)
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
+    res.status(200).json({ user: user._id })
+  } catch (err) {
+    const errors = handleErrors(err)
+    res.status(400).json({ errors });
+  }
 };
-
-// export {
-//   signup_get,
-//   login_get,
-//   signup_post,
-//   login_post
-// }
--- models/User.ts
+++ models/User.ts
@@ -1,31 +1,52 @@
-import mongoose from "mongoose";
-import bcrypt from 'bcrypt'
-const { Schema } = mongoose
+import mongoose, { Document } from 'mongoose';
+import bcrypt from 'bcrypt';
 
-const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+// export interface Document {
+//   email: string;
+//   password: string;
+// }
 
-const userSchema = new Schema({
+export interface UserModel extends mongoose.Model<Document> {
+  login(email: string, password: string): Document;
+}
+
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'Please enter your email'],
     unique: true,
     lowercase: true,
     // validate: [(val) => emailRegex.test(val), 'Please enter a valid email'],
-    trim: true,
-    match: [emailRegex, 'Please enter a valid email']
+    match: [emailRegex, 'Please enter a valid email'],
   },
   password: {
     type: String,
     required: [true, 'Please enter your password'],
     minlength: [6, 'Password must be at least 6 characters'],
-    select: false
+  },
+});
+
+userSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (isPasswordCorrect) {
+      return user;
+    }
+    throw Error('Incorrect password');
   }
-})
+  throw Error('Incorrect email');
+};
 
 userSchema.pre('save', async function (next) {
-  const salt = await bcrypt.genSalt()
-  this.password = await bcrypt.hash(this.password, salt)
-  next()
-})
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
-export const User = mongoose.model('user', userSchema)
+export const User: UserModel = mongoose.model<Document, UserModel>(
+  'user',
+  userSchema,
+);

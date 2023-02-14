d63d2f078b22f60199c77c51c44b22c603c7cd69
Constantin <konstantindanilov19@hotmail.com>
2023-02-14 10:40:48 +0200

    created the logout option and made it work

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -7,11 +7,13 @@ import {
   signup_post,
   login_get,
   login_post,
+  logout_get
 } from '../controllers/authControllers';
 
 router.get('/signup', signup_get)
 router.post('/signup', signup_post)
 router.get('/login', login_get)
 router.post('/login', login_post)
+router.get('/logout', logout_get)
 
 export default router;
